cd7be7adc37ebb102cb54171f3a1fb87f65b2f54
ibah <109962513+ibah111@users.noreply.github.com>
2025-03-04 20:46:05 +0300

    feat: add to common pool

--- src/modules/databases/sqlite/models/index.ts
+++ src/modules/databases/sqlite/models/index.ts
@@ -1,5 +1,6 @@
 import { ModelCtor } from 'sequelize-typescript';
 import User from './user.balance';
 import Account from './account.model';
+import Payment from './payment.model';
 
-export const models: ModelCtor[] = [User, Account];
+export const models: ModelCtor[] = [User, Account, Payment];
